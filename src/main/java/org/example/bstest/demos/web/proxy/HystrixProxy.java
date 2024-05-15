c7ea9a8c6420f767cb6b390756aca4f6c082a46b
wuyuhang017 <wuyuhang017@ke.com>
2024-05-15 15:43:00 +0800

    引入 kafka批处理及新增部分组件

--- src/main/java/org/example/bstest/demos/web/proxy/HystrixProxy.java
+++ src/main/java/org/example/bstest/demos/web/proxy/HystrixProxy.java
@@ -7,22 +7,29 @@ import com.netflix.hystrix.strategy.metrics.HystrixMetricsPollerFactory;
 
 public class HystrixProxy {
 
-    // ...
 
-    // 假设你有一个Hystrix命令的key
-    HystrixCommandKey commandKey = HystrixCommandKey.Factory.asKey("MyCommand");
 
-    // 获取HystrixCommandMetrics实例
-    HystrixCommandMetrics metrics = HystrixCommandMetrics.getInstance(commandKey,
-            HystrixMetricsPollerFactory.getInstance().getDefaultPoller());
+    public doProcess() {
 
-    // 获取并解析metrics
-    HealthCounts healthCounts = metrics.getHealthCounts();
-System.out.println("Requests: " + healthCounts.getTotalRequests());
+        // if有一个Hystrix命令的key
+        HystrixCommandKey commandKey = HystrixCommandKey.Factory.asKey("MyCommand");
+
+        // 获取HystrixCommandMetrics实例
+        HystrixCommandMetrics metrics = HystrixCommandMetrics.getInstance(commandKey,
+                HystrixMetricsPollerFactory.getInstance().getDefaultPoller());
+
+        // 获取并解析metrics
+        HystrixCommandMetrics.HealthCounts healthCounts = metrics.getHealthCounts();
+
+        System.out.println("Requests: " + healthCounts.getTotalRequests());
         System.out.println("Successes: " + healthCounts.getSuccesses());
         System.out.println("Failures: " + healthCounts.getFailures());
 
-// 你也可以获取其他类型的metrics，如执行时间百分比、线程池metrics等
+
+
+
+    }
+
 
 
 }
